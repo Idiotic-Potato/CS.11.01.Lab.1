5c957d306b296045af9179fad99b582d94a886c4
James Yan <jamesy2027@student.cis.edu.hk>
2024-08-23 12:47:47 +0800

    Lab1 Complete

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
-    }
-}
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int age= scanner.nextInt();
+        System.out.println(age+"-year olds should date somebody who is at least "+ ((age / 2)+7)+" years old");
+        scanner.close();}}
+
+
